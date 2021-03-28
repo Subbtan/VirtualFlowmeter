78793248d4c36679d25f8e2b247d7c6338129bf5
subbtan <1484710794@qq.com>
2021-03-28 17:13:25 +0800

    initialization

---
+++ Waterpump.py
@@ -0,0 +1,37 @@
+import numpy as np
+import scipy as sc
+import matplotlib as mpl
+
+
+class Waterpump:
+
+    def __init__(self, name, coe_H, coe_N, coe_eta):
+        # 定义水泵 包含水泵名称、三个特性曲面 十参数
+        # H/N/eta=f(Q,n)
+
+        self.coe_eta = np.array(10)
+        self.coe_H = np.array(10)
+        self.coe_N = np.array(10)
+
+        self.name = name
+        self.coe_H = coe_H
+        self.coe_N = coe_N
+        self.coe_eta = coe_eta
+
+        pass
+'''
+    def GetData(self, Q, n, Type):
+       # ans = f(Q, n)
+
+
+        # 根据输入流量与转速，返回H/N/eta的数值
+
+        return ans
+'''
+
+print('1')
+
+def prt():
+    print("hello world!")
+
+    pass
---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import scipy as sc
+import matplotlib as mpl
+import Waterpump
+
+
+#获得数据
+    
