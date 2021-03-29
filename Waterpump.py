9350724007b24fd677978d688b63c0ee2bc20732
subbtan <1484710794@qq.com>
2021-03-29 23:30:18 +0800

    3.29

--- Waterpump.py
+++ Waterpump.py
@@ -6,12 +6,13 @@ import matplotlib as mpl
 class Waterpump:
 
     def __init__(self, name, coe_H, coe_N, coe_eta):
+        # coe_H coe_N 为
         # 定义水泵 包含水泵名称、三个特性曲面 十参数
         # H/N/eta=f(Q,n)
 
-        self.coe_eta = np.array(10)
-        self.coe_H = np.array(10)
-        self.coe_N = np.array(10)
+        self.coe_eta = np.zeros(10)
+        self.coe_H = np.zeros(10)
+        self.coe_N = np.zeros(10)
 
         self.name = name
         self.coe_H = coe_H
