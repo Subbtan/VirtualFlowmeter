7bb7aee23a0eb830738e18c118a47fe0bd9d3b7d
subbtan <1484710794@qq.com>
2021-04-09 12:02:56 +0800

    完成4阶拟合

--- core/CurveFitting.py
+++ core/CurveFitting.py
@@ -3,6 +3,7 @@ import scipy as sc
 import scipy.optimize as opt
 import csv
 import matplotlib.pyplot as plt
+from scipy.optimize.minpack import leastsq
 
 # 导入数据
 
@@ -61,27 +62,48 @@ def residuals(p,X=data_Q,Y=data_H):
 X = data_Q
 Y = data_H
 
-def residuals2(p):
-    a1,a2,a3 = p
-    r = Y - (a1*X**2 + a2*X**1 + a3)
-    return r
+#
+def PolyResiduals(p,x,y):
+    fun = np.poly1d(p)
+    return y - fun(x)
 
-def residuals3(p):
-    a1,a2,a3,a4 = p
-    r = Y - (a1*X**3 + a2*X**2 + a3*X + a4)
-    return r
+def PowerResiduals(p,x,y):
+    a,b,c = p
+    return y - a*x**b + c
 
-# 最小二乘求系数
 
-r = opt.leastsq(residuals2,[1,0,1])
+def fitting(p):
+    if p >= 1:
+        pars = np.random.rand(p+1)
+        r = leastsq(PolyResiduals,pars,args=(X,Y))
+        return r[0]  #返回系数
+    
+    elif p == -1:
+        pars = np.random.rand(3)
+        pars = [-1.488e-07,4.983,11]
+        r = leastsq(PowerResiduals,pars,args=(X,Y))
+        return r[0]
 
-a1,a2,a3 = r[0]
+# 结果分析
+def result(p):
+    if p >= 1:
+        coe = fitting(p)
+        fun = np.poly1d(coe)
 
-Yfit = a1*X**2 + a2*X**1 + a3
+        plt.plot(data_Q,data_H,'r')
+        plt.plot(data_Q,fun(data_Q),'b')
+        plt.show()
 
-ccpfh = sum(residuals2(r[0])**2) #残差平方和
+    if p == -1:
+        coe = fitting(p)
+        a,b,c = coe
+        y = a*data_Q**b + c
+        plt.plot(data_Q,data_H,'r')
+        plt.plot(data_Q,y,'b')
+        plt.show()
 
-def draw2():
-    plt.plot(data_Q,data_H,'r')
-    plt.plot(data_Q,Yfit,'b')
-    pass
+# Yfit = a1*X**2 + a2*X**1 + a3
+
+# ccpfh = sum(residuals(r[0])**2) #残差平方和
+
+
